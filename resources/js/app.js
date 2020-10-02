ecaf4c201503e643392392d72a2fa2d0bd249af0
Roshan Elvitigala <roshanrox911@gmail.com>
2020-10-02 12:31:31 +1000

    added json read

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,38 @@
-console.log("OpenMarketsGroup");
+$(document).ready(function () {
+  console.log("OpenMarketsGroup");
+  let url = "https://roshanlakmal.github.io/practical_test/data.json";
+
+  /*  Render Card List */
+  //Get JSON data from data.json
+  $.getJSON(url, function (data) {
+    //Loop through JSON array and update the item information
+    $.each(data, function (index, value) {
+      let imageName = value.title.toLowerCase();
+      $(".cards").append(
+        "<div class='card'>" +
+          "<img class='inner-img' src='resources/img/" +
+          imageName +
+          ".png'" +
+          " alt='" +
+          imageName +
+          "'/>" +
+          "<h5 class='item-name'>" +
+          value.title +
+          "</h5>" +
+          "<p class='item-price'>" +
+          value.price +
+          "</p>" +
+          "</div>"
+      );
+    });
+  }).catch((err) => {
+    /* Error checking */
+    alert("something went wrong Error Code - " + err.status);
+  });
+  /*  End Render Card List */
+
+  /* Loading Animation*/
+  $(window).on("load", function () {
+    $("#cover").fadeOut(1750);
+  });
+});
